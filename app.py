c0dcfb8580355c3ad775b7928afa4577f162223b
Sebastián Aranda Ovallle <sebastian.aranda.o@gmail.com>
2024-05-22 19:12:19 -0400

    primer paso

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+page_title="Flores Eternas Gigi"
+page_icon="🎉"
+layout="centered"
+st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
+
+# Define CSS styles
+with open('./assets/css/styles.css') as css:
+    st.markdown(f"<style>{css.read()}</style>", unsafe_allow_html=True)
+
+st.image('./assets/flor.jpeg','Flores Eternas Gigi',width=200)
+st.title('Flores Eternas Gigi')
+st.text('Venta de Flores eternas individuales y por Ramos, con todo el amor del mundo')
+
+option_menu(menu_title=None,options=["Reservar","Imagenes","Detalles"],icons=["bookmark","bi-flower1","bi-ticket-detailed"])
